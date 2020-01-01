f783e8853b5f7d4ef966783628f0fd86c8144137
Marvin Lee <43216761+malee31@users.noreply.github.com>
2019-12-31 20:57:19 -0800

    I CAN READ POST DATA NOW

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ const path = require("path");
 
 const test = require("./spreadsheet.js");
 
+app.use(express.urlencoded());
+
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname + "/index.html"));
+	res.sendFile(path.resolve(__dirname + "/index.html"));
 });
 
 app.post('/', function (req, res) {
-    test.testPost();
-    test.pasteName();
-    res.send("Success");
+	test.testPost();
+	test.pasteName();
+	/*console.log(req);
+	console.log("Next: ");
+	console.log(res);
+	console.log("Fin");
+	console.log(req.body);
+	console.log("Bodied");*/
+	console.log(req.body.testInput);
+	res.send("Success");
 })
 
 app.listen(8080);
