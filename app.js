a6503f4bfe210dd50315a6cc59a344094bc8cffc
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-01-01 21:09:35 -0800

    Data retrieval and moving to public folder successful

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const path = require("path");
 const test = require("./spreadsheet.js");
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	res.sendFile(path.resolve(__dirname + "/public"));
+	res.sendFile(path.resolve(__dirname + "/public/html/index.html"));
 });
 
 app.post('/', function (req, res) {
 	test.testPost();
 	test.pasteName();
-	console.log(req.body.testInput);
-	res.send("Success");
+	console.log(req.body);
+	res.send("Success\n" + JSON.stringify(req.body));
 })
 
 app.listen(8080);
