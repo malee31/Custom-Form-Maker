1e5a449057b66dbdf7ec52eb254ada7608f83178
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-01-19 21:59:51 -0800

    Commenting app.js. Next: making it so that the spreadsheetID actually means something

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
+//ExpressJS imports
 const express = require('express');
 const app = express();
 const path = require("path");
 
+//Imports module imports from spreadsheet.js
 const test = require("./spreadsheet.js");
 
+//Allows parsing of data in requests
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use('*/css',express.static('public/css'));
-app.use('*/js',express.static('public/js'));
+//Serving static css and js files in index.html
+app.use('*/css', express.static('public/css'));
+app.use('*/js', express.static('public/js'));
 
 //Not used... I used the one above which probably isn't right. Will fix later.
 app.use("/static", express.static(path.resolve(__dirname, "public")));
 
+//Sends html file when the page is accessed
 app.get("/", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "views/index.html"));
 });
 
 app.post("/", (req, res) => {
-	test.pasteName();
+	//test.pasteName();
 	const info = req.body;
+
+	//Invalid Request error on empty POST request
 	if(Object.entries(info).length === 0 && info.constructor === Object)
 	{
+		//Unprocessable Entity
 		res.sendStatus(422);
 	}
 	else if(info.id)
 	{
+		//Runs if we are retrieving spreadsheet form
 		test.getHeaders(info.id).then(headers => {
 			//console.log(headers);
+			//returns the headers to site as a json file to be parsed
 			res.json(headers);
 		},
 		err => {
+			//Unprocessable Entity - caused usually by invalid spreadsheet ids
 			console.log(`Error: ${err}`);
 			res.sendStatus(422);
 		});
 	}
 	else
 	{
+		//Submitting data
 		//console.log(info);
 		test.newRow(info).then(output => {
 			console.log(output);
