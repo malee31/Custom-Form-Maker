b2f5e22b6ebfd1de2aa77083c3ab946fa55f47ac
Marvin Lee <43216761+malee31@users.noreply.github.com>
2021-02-16 12:03:48 -0800

    Front-end code now able to extract values from inputs

--- static/js/submit.js
+++ static/js/submit.js
@@ -25,6 +25,31 @@ function toggleErrorBox(show, title, desc) {
 }
 
 /**
+ * Collects all the data from form elements and assigns them to an object
+ * @param {Object} targetObject Object reference to assign the form data to
+ * @param {HTMLFormElement} form The form to collect data from
+ */
+function collectFormData(targetObject, form) {
+	for(const input of form.querySelectorAll("input")) {
+		if(!input.dataset.columnname || targetObject[input.dataset.columnname]) continue;
+		if(input.type === "checkbox") {
+			targetObject[input.dataset.columnname] = [];
+			form.querySelectorAll(`input[type="checkbox"][name=${input.name}]`)
+				.forEach(item => {
+					if(item.checked) targetObject[input.dataset.columnname].push(item.value);
+				});
+		} else if(input.type === "radio") {
+			form.querySelectorAll(`input[type="radio"][name=${input.name}]`)
+				.forEach(item => {
+					if(item.checked) targetObject[input.dataset.columnname] = item.value;
+				});
+		} else {
+			targetObject[input.dataset.columnname] = input.value;
+		}
+	}
+}
+
+/**
  * Handles all the overriding of the mainForm.
  */
 function mainFormOverride() {
@@ -34,13 +59,11 @@ function mainFormOverride() {
 
 		const data = {formId: document.getElementById("formId").value};
 
-		for(const input of mainForm.querySelectorAll("input")) {
-			if(input.dataset.columnname) data[input.dataset.columnname] = input.value;
-		}
+		collectFormData(data, mainForm);
 
 		const req = new XMLHttpRequest();
 		req.addEventListener("load", event => {
-			if(event.target.status !== 422) {
+			if(event.target.status === 200) {
 				mainForm.parentNode.removeChild(mainForm);
 			}
 
