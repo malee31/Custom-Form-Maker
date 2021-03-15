f54ae126b6e3e4f59c32b8c4a7e5c102dadae2b4
Marvin Lee <43216761+malee31@users.noreply.github.com>
2021-03-14 17:57:54 -0700

    Refactored code so that elements will not need to be queryselected every time

--- static/js/creation.js
+++ static/js/creation.js
@@ -42,6 +42,27 @@ window.addEventListener("load", () => {
 	});
 });
 
+function createToolOverride(e) {
+	if(e) e.preventDefault();
+	console.log("Creating New Input");
+	const data = fetchCreateToolValues();
+	requestTemplate(creationInputs.template.value).then(template => {
+		const uuid = generateUUID();
+		const rendered = parseHTMLString(ejs.render(template, {inputOptions: cleanData(data, uuid)}))[0];
+		const elementMap = createRenderWrapper();
+		elementMap.renderWrapper.dataset.uuid = uuid;
+		addEditElementReferences(elementMap);
+		attachEditListeners(rendered, elementMap);
+
+		elementMap.replace.replaceWith(rendered);
+		formAppend(elementMap.renderWrapper);
+		elementMap.data = data;
+		dataMap[uuid] = elementMap;
+		GeneratedData.headers.push(uuid);
+		console.log("New Input Added");
+	});
+}
+
 function editableListeners(editableElem) {
 	editableElem.addEventListener("keydown", e => {
 		editableContentFilter(e.target, e.target.innerText.replace(/\s/g, " "));
@@ -50,7 +71,6 @@ function editableListeners(editableElem) {
 		editableContentFilter(e.target, e.target.innerText.replace(/\s/g, " "), false);
 	});
 	editableElem.addEventListener("paste", e => {
-		// TODO: Manually implement pasting so that text/plain format is enforced
 		const savedSelection = cutSelection();
 		const clippedText = e.clipboardData.getData("text/plain").replace(/\s/g, " ");
 		e.target.innerText = `${e.target.innerText.substring(0, savedSelection.start)}${clippedText}${e.target.innerText.substring(savedSelection.end)}`;
@@ -89,25 +109,6 @@ function editableContentFilter(targetElem, filteredText, restore = true) {
 	if(restore) restoreSelection(targetElem, savedRange);
 }
 
-function createToolOverride(e) {
-	if(e) e.preventDefault();
-	console.log("Creating New Input");
-	const data = fetchCreateToolValues();
-	requestTemplate(creationInputs.template.value).then(template => {
-		const uuid = generateUUID();
-		const rendered = parseHTMLString(ejs.render(template, {inputOptions: cleanData(data, uuid)}))[0];
-		const {renderWrapper, editor, replace} = createRenderWrapper();
-		renderWrapper.dataset.uuid = uuid;
-		attachEditListener(rendered, editor, renderWrapper);
-
-		replace.replaceWith(rendered);
-		formAppend(renderWrapper);
-		GeneratedData.headers.push(data);
-		dataMap[uuid] = GeneratedData.headers.length - 1;
-		console.log("New Input Added");
-	});
-}
-
 function fetchCreateToolValues(targetObj = makeData()) {
 	targetObj.displayName = creationInputs.labelValue.value;
 	targetObj.type = creationInputs.template.value;
@@ -118,20 +119,24 @@ function fetchCreateToolValues(targetObj = makeData()) {
 	return targetObj;
 }
 
-function fetchEditorValues(editWrapperElement, uuid) {
-	const editingHeader = GeneratedData.headers[dataMap[uuid]];
-	editingHeader.displayName = editWrapperElement.querySelector(".label-editor").value;
-	editingHeader.placeholderText = editWrapperElement.querySelector(".placeholder-editor").value;
-	editingHeader.defaultValue = editWrapperElement.querySelector(".default-value-editor").value;
-	editingHeader.required = editWrapperElement.querySelector(".required-editor").checked;
+function fetchEditorValues(elementMap) {
+	// For lookup by uuid
+	elementMap = typeof elementMap === "string" ? dataMap[elementMap] : elementMap;
+	const editorElements = elementMap.editorElements;
+	const editingHeader = elementMap.data;
+	editingHeader.displayName = editorElements.labelValue.value;
+	editingHeader.placeholderText = editorElements.placeholderValue.value;
+	editingHeader.defaultValue = editorElements.defaultValue.value;
+	editingHeader.required = editorElements.requiredValue.checked;
 	return editingHeader;
 }
 
-function updatePreview(editWrapperElement) {
-	const updatedData = fetchEditorValues(editWrapperElement, editWrapperElement.dataset.uuid);
+function updatePreview(elementMap) {
+	const updatedData = fetchEditorValues(elementMap);
 	requestTemplate(updatedData.type).then(template => {
-		const rendered = parseHTMLString(ejs.render(template, {inputOptions: cleanData(updatedData, editWrapperElement.dataset.uuid)}))[0];
-		editWrapperElement.querySelector(".pseudo-label").replaceWith(rendered);
+		const rendered = parseHTMLString(ejs.render(template, {inputOptions: cleanData(updatedData, elementMap.renderWrapper.dataset.uuid)}))[0];
+		elementMap.renderWrapper.querySelector(".pseudo-label").replaceWith(rendered);
+		attachEditOpenerListeners(rendered, elementMap);
 	});
 }
 
@@ -154,37 +159,45 @@ function createRenderWrapper() {
 	const renderWrapperClone = renderWrapperTemplate.content.cloneNode(true);
 	const renderElements = {
 		renderWrapper: renderWrapperClone.querySelector(".render-wrapper"),
-		editor: renderWrapperClone.querySelector(".edit-controls-wrapper"),
+		editorWrapper: renderWrapperClone.querySelector(".edit-controls-wrapper"),
 		closeEditor: renderWrapperClone.querySelector(".close-editor-button"),
 		replace: renderWrapperClone.querySelector(".render-replace")
 	}
 
 	renderElements.closeEditor.addEventListener("click", () => {
 		renderElements.renderWrapper.classList.remove("edit-mode");
-		renderElements.editor.classList.add("dimensionless");
+		renderElements.editorWrapper.classList.add("dimensionless");
 	});
 
 	return renderElements;
 }
 
-function attachEditListener(previewRender, editControls, wrapper) {
+function attachEditOpenerListeners(previewRender, elementMap) {
 	previewRender.addEventListener("click", () => {
-		if(!wrapper.classList.contains("edit-mode")) {
-			wrapper.classList.add("edit-mode");
-			editControls.classList.remove("dimensionless");
+		if(!elementMap.renderWrapper.classList.contains("edit-mode")) {
+			elementMap.renderWrapper.classList.add("edit-mode");
+			elementMap.editorWrapper.classList.remove("dimensionless");
 		}
 	});
+}
 
-	for(const input of editControls.querySelectorAll("input")) {
-		input.addEventListener("input", () => {
-			updatePreview(wrapper);
-		});
-		input.addEventListener("change", () => {
-			updatePreview(wrapper);
-		});
-	}
+function attachEditListeners(previewRender, elementMap) {
+	attachEditOpenerListeners(previewRender, elementMap);
 
-	return previewRender;
+	const updatePreviewForWrapper = () => {updatePreview(elementMap)};
+	elementMap.editorElements.labelValue.addEventListener("input", updatePreviewForWrapper);
+	elementMap.editorElements.defaultValue.addEventListener("input", updatePreviewForWrapper);
+	elementMap.editorElements.placeholderValue.addEventListener("input", updatePreviewForWrapper);
+	elementMap.editorElements.requiredValue.addEventListener("change", updatePreviewForWrapper);
+}
+
+function addEditElementReferences(renderMap) {
+	renderMap.editorElements = {
+		labelValue: renderMap.editorWrapper.querySelector(".label-editor"),
+		defaultValue: renderMap.editorWrapper.querySelector(".default-value-editor"),
+		placeholderValue: renderMap.editorWrapper.querySelector(".placeholder-editor"),
+		requiredValue: renderMap.editorWrapper.querySelector(".required-editor"),
+	};
 }
 
 function makeData() {
@@ -213,4 +226,17 @@ function formAppend(newNode) {
 
 function generateUUID() {
 	return `Pseudo-UUID-${uuidCounter}`;
+}
+
+function finalizeGeneratedData() {
+	for(let uuidNum = 0; uuidNum < GeneratedData.headers.length; uuidNum++) {
+		const uuid = GeneratedData.headers[uuidNum];
+		if(typeof uuid === "object") {
+			console.log("Already finalized.");
+			console.log(uuid);
+			continue;
+		}
+		GeneratedData.headers[uuidNum] = dataMap[uuid].data;
+	}
+	return GeneratedData;
 }
