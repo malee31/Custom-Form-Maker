2156de5843b4ab3431ac323431a507570ca4e7b4
Marvin Lee <43216761+malee31@users.noreply.github.com>
2021-05-13 12:13:49 -0700

    Merged some ejs files and some js files together. Merged js files will have their sections of code check if an element exists before running

--- static/js/mobileResize.js
+++
@@ -1,4 +0,0 @@
-window.onresize = () => {
-	document.getElementById("pad").style.height = `${Math.max(0, document.querySelector("#control").clientHeight - window.innerHeight)}px`;
-	// document.querySelector("input[type=submit]").value = document.querySelector("#control").clientHeight + ", " + window.innerHeight;
-};
--- static/js/utilities.js
+++ static/js/utilities.js
@@ -1,4 +1,17 @@
 const errBox = document.getElementById("errorBox");
+const padElem = document.getElementById("pad");
+const controlElem = document.getElementById("control");
+
+/**
+ * Toggles the loading wheel by hiding or showing it
+ * @param {boolean} [show = false] Whether to show or hide the loader. Defaults to hiding it
+ */
+function toggleLoader(show) {
+	const loader = document.querySelector(".loadWheel");
+	if(!loader) return console.log(`No loader to toggle ${show ? "on" : "off"}`);
+	if(typeof show !== "boolean") show = loader.style.visibility === "hidden";
+	loader.style.visibility = show ? "visible" : "hidden";
+}
 
 /**
  * Toggles the error window by hiding or showing it. Also sets the text if provided. Doubles as a modal
@@ -8,6 +21,7 @@ const errBox = document.getElementById("errorBox");
  * @param {boolean} [innerHTMLMode = false] If set to a true value, will insecurely set the innerHTML of the description instead of the innerText
  */
 function toggleErrorBox(show, title, desc, innerHTMLMode = false) {
+	if(!errBox) return console.log(`No Error Box Element to Toggle ${show ? "on" : "off"}`);
 	if(typeof show !== "boolean") show = errBox.style.opacity === "0";
 	if(show) {
 		errBox.style.display = "flex";
@@ -25,3 +39,7 @@ function toggleErrorBox(show, title, desc, innerHTMLMode = false) {
 		else document.getElementById("errorDesc").innerText = desc;
 	}
 }
+
+if(padElem && controlElem) window.onresize = () => {
+	padElem.style.height = `${Math.max(0, controlElem.clientHeight - window.innerHeight)}px`;
+};
